e972acd1afb3944a5af342d8a505555f5b0376d8
Christian Schildt <38693641+ChrisArticy@users.noreply.github.com>
2018-07-26 18:00:04 +0200

    added ArticyReference actor component

---
+++ ArticyImporter/Source/ArticyRuntime/Private/ArticyReference.cpp
@@ -0,0 +1,28 @@
+//  
+// Copyright (c) articy Software GmbH & Co. KG. All rights reserved.  
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.  
+//
+#include "ArticyRuntimePrivatePCH.h"
+
+#include "ArticyReference.h"
+
+
+UArticyReference::UArticyReference()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+bool UArticyReference::IsValid()
+{
+	return Reference.GetId().Get() != 0;
+}
+
+UArticyPrimitive* UArticyReference::GetObject(const UObject* WorldContext)
+{
+	return Reference.GetObject<UArticyPrimitive>(WorldContext);
+}
+
+void UArticyReference::SetReference(UArticyPrimitive* Object)
+{
+	Reference.SetReference(Object);
+}
