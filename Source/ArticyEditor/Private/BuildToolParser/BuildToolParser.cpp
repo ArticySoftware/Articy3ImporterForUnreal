73b0abbe2c285f578bf6624579eb64d858a202e9
Alewinn <jean-christophe@industriesindigenes.com>
2022-11-24 00:39:48 +0100

    Fixed Unreal C# build file parser to be much more robust.

--- Source/ArticyEditor/Private/BuildToolParser/BuildToolParser.cpp
+++ Source/ArticyEditor/Private/BuildToolParser/BuildToolParser.cpp
@@ -12,51 +12,106 @@ BuildToolParser::BuildToolParser(const FString& filePath)
 
 bool BuildToolParser::VerifyArticyRuntimeRef()
 {
-	// Open the Path file and read its content
-	// Then parse the file to find a "ArticyRuntime" Reference inside the public dependency array
-	if (!FFileHelper::LoadANSITextFileToStrings(*Path,NULL ,FileCodeLines ))
+	// Open the Path file and read its content as one line string
+	FString fileString;
+	if (!FFileHelper::LoadFileToString(fileString, *Path))
 	{
 		UE_LOG(LogArticyEditor, Error, TEXT("Failed to load file '%s' to string"), *Path);
 		return false;
 	}
 
-	PublicDependencyIndex = 0;
-	for (auto &Str : FileCodeLines)
-	{
-		if (Str.Contains(TEXT("PublicDependencyModuleNames.AddRange"), ESearchCase::CaseSensitive))
-		{
-			Str.RemoveSpacesInline();
-			Str.ReplaceInline(TEXT("\t"), TEXT(""));
-			Str.ReplaceInline(TEXT("PublicDependencyModuleNames.AddRange(newstring[]{"),TEXT(""));
-			Str.ReplaceInline(TEXT( "});"),TEXT(""));
-			Str.ParseIntoArray(PublicDependencyModulesNames, TEXT(","));
-
-			if (PublicDependencyModulesNames.Find(TEXT("\"ArticyRuntime\"")) != INDEX_NONE)
-				return true;
-			else
-				return false;
-		}
-		PublicDependencyIndex++;
-	}
-
-	return false;
+	// Extract PublicDependencyModuleNames c# code lines
+	FString UncommentedString = RemoveComments(fileString);
+	return CheckReferences(UncommentedString);
 }
 
 void BuildToolParser::AddArticyRuntimmeRef()
 {
-	PublicDependencyModulesNames.Emplace(TEXT("\"ArticyRuntime\""));
-	
-	FString newDepArray = "\t\tPublicDependencyModuleNames.AddRange(new string[] {";
-	for (int32 Index = 0; Index != PublicDependencyModulesNames.Num(); ++Index)
+	// Open the Path file and read its content as one line string
+	FString fileString;
+	if (!FFileHelper::LoadFileToString(fileString, *Path))
 	{
-		if(Index < PublicDependencyModulesNames.Num()-1)
-			newDepArray += PublicDependencyModulesNames[Index] + ",";
-		else
-			newDepArray += PublicDependencyModulesNames[Index];
+		UE_LOG(LogArticyEditor, Error, TEXT("Failed to load file '%s' to string"), *Path);
+		return;
 	}
-	newDepArray += "});";
+	
+	// Same logic than removng comments => TODO : generalize the whole process...
+	FString MatchRule(TEXT("/\\*(.*?)\\*/"));							// blockComments
+	MatchRule.Append(TEXT("|//(.*?)\\r?\\n"));							// lineComments
+	MatchRule.Append(TEXT("|\"\"((\\\\[^\\n]|[^""\\n])*)\"\""));		// strings
+	MatchRule.Append(TEXT("|@(\"\"[^\"\"]*\"\")+"));					// C# verbatimStrings
+	MatchRule.Append(TEXT("|PublicDependencyModuleNames[\\s\\S]*?\\{"));	// Dependency array
+	
+	const FRegexPattern RegexPattern(*MatchRule);
 
-	FileCodeLines[PublicDependencyIndex] = newDepArray;
+	FRegexMatcher RegexMatcher(RegexPattern, fileString);
 
-	FFileHelper::SaveStringArrayToFile(FileCodeLines, *Path);
+	while (RegexMatcher.FindNext())
+	{
+		FString TestLine = fileString.Mid(RegexMatcher.GetMatchBeginning(),RegexMatcher.GetMatchEnding() - RegexMatcher.GetMatchBeginning());
+		if (TestLine.StartsWith("PublicDependencyModuleNames") )
+		{
+			fileString.InsertAt(RegexMatcher.GetMatchEnding(), TEXT("\"ArticyRuntime\","));
+		}
+	}
+
+	FFileHelper::SaveStringToFile(fileString, *Path);	
+}
+
+FString BuildToolParser::RemoveComments(FString line)
+{
+	FString StrReturn(line);
+	FString CommentMatchRule(TEXT("/\\*(.*?)\\*/"));					// blockComments
+	CommentMatchRule.Append(TEXT("|//(.*?)\\r?\\n"));					// lineComments
+	CommentMatchRule.Append(TEXT("|\"\"((\\\\[^\\n]|[^""\\n])*)\"\"")); // strings
+	CommentMatchRule.Append(TEXT("|@(\"\"[^\"\"]*\"\")+"));				// C# verbatimStrings
+	
+	const FRegexPattern CsharpComments(*CommentMatchRule);
+
+	FRegexMatcher CommentsRemover(CsharpComments, StrReturn);
+	int64 offset = 0;
+	while (CommentsRemover.FindNext())
+	{
+		int32 MatchBegin = CommentsRemover.GetMatchBeginning() - offset;
+		
+		if (StrReturn[MatchBegin] == '/')
+		{
+			if (MatchBegin + 1 <= StrReturn.Len() &&
+				(
+					StrReturn[MatchBegin + 1] == '/' ||
+					StrReturn[MatchBegin + 1] == '*'
+				)
+			)
+			{
+				// Regex found a comment => strip the whole match from string
+				StrReturn.RemoveAt(MatchBegin, CommentsRemover.GetMatchEnding() - CommentsRemover.GetMatchBeginning());
+				offset = offset + (CommentsRemover.GetMatchEnding() - CommentsRemover.GetMatchBeginning());
+			}
+		}
+		// => else, do not touch litteral strings (regex matched on " or @)
+	}
+	
+	return StrReturn;
+}
+
+bool BuildToolParser::CheckReferences(FString line)
+{
+	FString PDependency(TEXT("PublicDependencyModuleNames"));	// Method to find
+	PDependency.Append(TEXT("[\\s\\S]*?"));						// Content
+	PDependency.Append(TEXT("\\}\\r?\\n*\\)\\r?\\n*;"));		// End delimiter
+	
+	const FRegexPattern PdependencyPattern(*PDependency);
+
+	FRegexMatcher DependencyExtractor(PdependencyPattern, line);
+	
+	while(DependencyExtractor.FindNext())
+	{
+		FString TestLine = line.Mid(DependencyExtractor.GetMatchBeginning(),DependencyExtractor.GetMatchEnding() - DependencyExtractor.GetMatchBeginning());
+		if(TestLine.Contains("ArticyRuntime"))
+		{
+			return true;
+		}
+	}
+	
+	return false;
 }
--- Source/ArticyEditor/Private/BuildToolParser/BuildToolParser.h
+++ Source/ArticyEditor/Private/BuildToolParser/BuildToolParser.h
@@ -19,6 +19,21 @@ public:
 private:
 	// Enforce using parameterized constructor
 	BuildToolParser() {};
+	
+	/**
+	 * @brief Return new string where single and multi line C# comments have been removed.
+	 * @param line : the string from which comments must be removed.
+	 * @return a new string where comments have been removed.
+	 */
+	FString RemoveComments(FString line);
+
+	/**
+	 * @brief Return a list of all dependency modules lines from a string
+	 * @param line : the string without comments  
+	 * @return a list of the dependency modules c# code lines
+	 */
+	bool CheckReferences(FString line);
+	
 	int PublicDependencyIndex;
 	FString Path = TEXT("");
 	TArray <FString> PublicDependencyModulesNames;
